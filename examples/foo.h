c3ea4a730416be893b9db7b659e3ac42b3dba257
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-04-24 18:30:01 +0200

    example

---
+++ examples/foo.h
@@ -0,0 +1,6 @@
+#ifndef EXAMPLES_CPP_FOO_H_
+#define EXAMPLES_CPP_FOO_H_
+
+int add(int x, int y);
+
+#endif  // EXAMPLES_CPP_FOO_H_
---
+++ examples/main.cc
@@ -0,0 +1,8 @@
+#include "examples/foo.h"
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int sum = add(x, y);
+    return sum;
+}
